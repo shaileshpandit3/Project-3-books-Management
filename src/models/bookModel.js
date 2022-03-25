ac8a827f66ee55cc1f88b01d577cc94acf90e7fd
shaileshpandit3 <shaileshpandit85@gmail.com>
2022-03-25 15:44:35 +0530

    book model added

---
+++ src/models/bookModel.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const bookSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        required: [true,'title is required'],
+        trim: true,
+        unique: true
+    },
+    excerpt: {
+        type: String,
+        trim: true,
+        required: [true,'excerpt is required']
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true,'userId is required'],
+        trim: true,
+        refs: 'user'
+    },
+    ISBN: {
+        type: String,
+        requireed: [true,'ISBN is required'],
+        trim: true,
+        unique: true
+    },
+    category: {
+        type: String,
+        trim: true,
+        required: [true,'category is required']
+    },
+    subcategory: {
+        type: String,
+        trim: true,
+        required: [true,'subcategory is required']
+    },
+    reviews: {
+        type: Number,
+        default: 0,
+        //comment: Holds number of reviews of this book
+    },
+    deletedAt: {
+        type: Date,
+        default: null
+        //when the document is deleted
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    releasedAt: {
+        type: Date,
+        required: [true,"Release date is required"]// format("YYYY-MM-DD")
+    },
+
+
+
+}, { timestamps: true })
+module.exports = mongoose.model('Book', bookSchema)
