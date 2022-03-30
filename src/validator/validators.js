ab04b6fa192ace8b62be53c327d72843e7358f81
satilasha <swatighosh2111@gmail.com>
2022-03-30 13:54:39 +0530

    update

--- src/validator/validators.js
+++ src/validator/validators.js
@@ -47,11 +47,8 @@ const isValidISBN = ( ISBN ) => {
 }
 
 const isValidReleasedAt = (releasedAt) => {
-    return /((\d{4}[\/-])(\d{2}[\/-])(\d{2}))/.test(releasedAt)
+    return /^(19|20)\d\d([- /.])(0[1-9]|1[012])\2(0[1-9]|[12][0-9]|3[01])$/.test(releasedAt)
 }
-// const isAllLetters = (val) => {
-//     return /^[A-Za-z]+$/.test(val)
-// }
 
 
 module.exports = {
